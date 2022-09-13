c75ed57d79c6b7d0a9018c6fc7fd98f2bce555fe
Rob Uhrig <105675520+TartanSpartan117@users.noreply.github.com>
2022-09-13 16:05:23 -0400

    Card columns applied to page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 const results = document.querySelector("#results");
 
-//console.log(e);
-
 async function asyncFetch(value) {
   const res = await fetch(`https://swapi.dev/api/${value}/`);
   const data = await res.json();
@@ -10,18 +8,17 @@ async function asyncFetch(value) {
 
 function displayResults(data, value) {
   let output = "";
-  console.log(data);
   if (value === "films") {
     data.results.forEach((item) => {
       output += `
         <div class="card p-3 m-3" style="opacity:.8">
-          <h4 class="card-title text-center">${item.title}</h4>
-          <div class="card-content">
-            <span style="text-decoration:underline">Producer</span>: ${item.producer}<br>
-            <span style="text-decoration:underline">Director</span>: ${item.director}<br>
-            <span style="text-decoration:underline">Release Date</span>: ${item.release_date}<br><br>
-            <p class=""text-center> ${item.opening_crawl}</p>
-          </div>
+           <h4 class="card-title text-center">${item.title}</h4>
+           <div class="card-content">
+              <span style="text-decoration:underline">Producer</span>: ${item.producer}<br>
+              <span style="text-decoration:underline">Director</span>: ${item.director}<br>
+              <span style="text-decoration:underline">Release Date</span>: ${item.release_date}<br><br>
+              <p class=""text-center> ${item.opening_crawl}</p>
+           </div>
         </div>
       `;
     });
@@ -35,9 +32,10 @@ function displayResults(data, value) {
           <div class="card-content">
             <span style="text-decoration:underline">Birth Year</span>: ${item.birth_year}<br>
             <span style="text-decoration:underline">Skin Color</span>: ${item.skin_color}<br>
+            <span style="text-decoration:underline">Hair Color</span>: ${item.hair_color}<br>
             <span style="text-decoration:underline">Eye Color</span>: ${item.eye_color}<br>
+            <span style="text-decoration:underline">Height</span>: ${item.height}<br>
             <span style="text-decoration:underline">Gender</span>: ${item.gender}<br>
-            <span style="text-decoration:underline">Species</span>: ${item.species}<br>
           </div>
         </div>
       `;
@@ -61,11 +59,32 @@ function displayResults(data, value) {
     });
   }
 
+  if (value === "starships") {
+    data.results.forEach((item) => {
+      output += `
+        <div class="card p-3 m-3" style="opacity:.8">
+          <h4 class="card-title text-center">${item.name}</h4>
+          <div class="card-content">
+            <span style="text-decoration:underline">Model</span>: ${item.model}<br>
+            <span style="text-decoration:underline">MGLT</span>: ${item.MGLT}<br>
+            <span style="text-decoration:underline">Hyperdrive Rating</span>: ${item.hyperdrive_rating}<br>
+            <span style="text-decoration:underline">Cargo Capacity</span>: ${item.cargo_capacity}<br>
+            <span style="text-decoration:underline">Consumables</span>: ${item.consumables}<br>
+            <span style="text-decoration:underline">Cost In Credits</span>: ${item.cost_in_credits}<br>
+            <span style="text-decoration:underline">Crew</span>: ${item.crew}<br>
+            <span style="text-decoration:underline">Passengers</span>: ${item.passengers}<br>
+          </div>
+        </div>
+      `;
+    });
+  }
+
   if (value === "vehicles") {
     data.results.forEach((item) => {
       output += `
         <div class="card p-3 m-3" style="opacity:.8">
           <h4 class="card-title text-center">${item.name}</h4>
+        <div class="card-body">
           <div class="card-content">
             <span style="text-decoration:underline">Model</span>: ${item.model}<br>
             <span style="text-decoration:underline">Vehicle Class</span>: ${item.vehicle_class}<br>
@@ -74,6 +93,7 @@ function displayResults(data, value) {
             <span style="text-decoration:underline">Cost In Credits</span>: ${item.cost_in_credits}<br>
             <span style="text-decoration:underline">Crew</span>: ${item.crew}<br>
             <span style="text-decoration:underline">Passengers</span>: ${item.passengers}<br>
+          </div>
           </div>
         </div>
       `;
